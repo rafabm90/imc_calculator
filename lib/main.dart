a86dfce61742f738e42cab4b59d50b1b94582a7d
rafabm90 <rafaelborges@arpiatecnologia.com>
2021-08-04 15:00:58 -0300

    Finishing imc calculator

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  String _infoText = "Informe seus dados";
+
+  void _resetField() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obsidade Grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obsidade Grau II (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 40.0) {
+        _infoText = "Obsidade Grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,13 +72,105 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _resetField(),
             icon: Icon(
               Icons.refresh,
             ),
           ),
         ],
       ),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Icon(
+                  Icons.person_outline,
+                  size: 120.0,
+                  color: Colors.lightBlue.shade300,
+                ),
+                CustomTextField(
+                  errorText: "Insira seu peso",
+                  labelText: "Peso (Kg)",
+                  controller: weightController,
+                ),
+                CustomTextField(
+                  errorText: "Insira sua altura",
+                  labelText: "Altura (cm)",
+                  controller: heightController,
+                ),
+                SizedBox(height: 15.0),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        Colors.lightBlue.shade300),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _calculate();
+                    }
+                  },
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+                SizedBox(height: 15.0),
+                Center(
+                  child: Text(
+                    _infoText,
+                    style: TextStyle(
+                      color: Colors.lightBlue.shade300,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String labelText;
+  final TextEditingController controller;
+  final String errorText;
+
+  const CustomTextField(
+      {Key? key,
+      required this.labelText,
+      required this.controller,
+      required this.errorText})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return errorText;
+        }
+      },
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: TextStyle(
+          color: Colors.lightBlue.shade300,
+        ),
+      ),
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.lightBlue.shade300,
+        fontSize: 20.0,
+      ),
+      controller: controller,
     );
   }
 }
