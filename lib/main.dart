84f18f66515df82e8e186858fca8fba94f55ee8f
rafabm90 <rafaelborges@arpiatecnologia.com>
2021-08-04 14:03:22 -0300

    Adding scaffold and appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(AppWidget());
 }
 
-
 class AppWidget extends StatelessWidget {
-  const AppWidget({ Key? key }) : super(key: key);
+  const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        backgroundColor: Colors.lightBlue.shade300,
+      ),
       home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
@@ -28,6 +28,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue.shade300,
+        title: Text("Calculadora IMC"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.refresh,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
